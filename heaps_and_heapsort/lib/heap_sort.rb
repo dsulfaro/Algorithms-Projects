8922eaa858d24adde0394f84244c6a2ed436c27b
Dan Sulfaro <dsulfaro1@gmail.com>
2016-11-29 10:23:55 -0800

    add comments

--- heaps_and_heapsort/lib/heap_sort.rb
+++ heaps_and_heapsort/lib/heap_sort.rb
@@ -3,10 +3,12 @@ require_relative "heap"
 class Array
   def heap_sort!
     heap = BinaryMinHeap.new
+    # heapify array
     while self.length > 0
       heap.push(self.shift)
     end
     sorted = []
+    # pop off heap elements into array
     while heap.count > 0
       self << heap.extract
     end
