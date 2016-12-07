56bbb7f3e512f3c38ca4474bb00b312bb32b0bd9
Dan Sulfaro <dsulfaro1@gmail.com>
2016-12-06 20:18:05 -0800

    finish Kahn's Algorithm

--- topological_sort/lib/graph.rb
+++ topological_sort/lib/graph.rb
@@ -1,11 +1,12 @@
 class Vertex
 
-  attr_accessor :value, :in_edges, :out_edges
+  attr_accessor :value, :in_edges, :out_edges, :in_degree
 
   def initialize(value)
     @value = value
     @in_edges = []
     @out_edges = []
+    @in_degree = 0
   end
 
 end
@@ -20,11 +21,13 @@ class Edge
     @cost = 1
     @to_vertex.in_edges << self;
     @from_vertex.out_edges << self;
+    @to_vertex.in_degree += 1
   end
 
   def destroy!
     @to_vertex.in_edges.delete(self)
     @from_vertex.out_edges.delete(self)
+    @to_vertex.in_degree -= 1
     @from_vertex, @to_vertex = nil
   end
 
--- topological_sort/lib/topological_sort.rb
+++ topological_sort/lib/topological_sort.rb
@@ -1,7 +1,39 @@
 require_relative 'graph'
-
+require 'byebug'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
+# def topological_sort(vertices)
+#
+#   degrees = {}
+#   sorted = []
+#   top_level = []
+#
+#   vertices.each do |vert|
+#     degrees[vert] = vert.in_edges.size
+#     top_level << vert if vert.in_edges.empty?
+#   end
+#
+#   until top_level.empty?
+#     current = top_level.pop
+#     sorted << current
+#     current.out_edges.each do |edge|
+#       degrees[edge.to_vertex] -= 1
+#       top_level << edge.to_vertex if degrees[edge.to_vertex] == 0
+#     end
+#   end
+#   sorted
+# end
+
 def topological_sort(vertices)
-  sorted = []
+  result = []
+  seen = []
+  vertices.each { |vert| depth_first_search(vert, result, seen) }
+  byebug
+  result
+end
+
+def depth_first_search(vertex, result, seen)
+  seen << vertex
+  vertex.out_edges.each { |edge| depth_first_search(edge.to_vertex, result, seen) unless seen.include?(edge.to_vertex) }
+  result.unshift(vertex)
 end
